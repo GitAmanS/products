177a8d9741d5e6342365dd0bfe3308420558db4a
Aman Shaikh <amanshaikh8624@gmail.com>
2023-11-11 01:51:28 -0800

    404

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const router = express.Router();
-const bodyParser = require('body-parser');
-router.get('/add-products', (req, res, next)=>{
+const path = require('path');
 
-    res.send(`<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>`);
+const express = require('express');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
-router.post('/product', (req, res, next)=>{
-    console.log(req.body);
-    res.send(`<h1>${req.body.title}</h1>`);
-    res.redirect('/');
-})
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
 
 module.exports = router;
